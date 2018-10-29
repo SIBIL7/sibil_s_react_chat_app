4df4093ff91e6c3bbcbc287b74b0ea4a3a2db628
SIBIL7 <sibil7beats@gmail.com>
2018-10-29 13:51:38 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const tokenUrl = 
+"https://us1.pusherplatform.io/services/chatkit_token_provider/v1/d3aaaffe-0090-4023-b159-2539620ba5f3/token";
+const instanceLocator = "v1:us1:d3aaaffe-0090-4023-b159-2539620ba5f3";
+
+export { tokenUrl, instanceLocator }
+
+
